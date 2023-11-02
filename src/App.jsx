c18fb8b8485771e9b2d7a24a252b60637eeb3c33
Lucas Lima <lucasrlima@gmail.com>
2023-11-02 18:15:01 +0000

    basic handleClick function added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import Button from './Button'
 export default 
 
 function App() {
+
+  function handleClick() {
+    return (
+      alert("Password Generated")
+    )
+  }
+
   return (
     <>
       <h1>Password Generator</h1>
-      <Button Text="Copy"></Button>
-      <Button Text="Generate"></Button>
+      <Button onClick={handleClick} Text="Copy"></Button>
+      <Button onClick={handleClick} Text="Generate"></Button>
     </>
   )
 }
