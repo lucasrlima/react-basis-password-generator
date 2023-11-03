9f5089e2562ac120c67eede4be10f554488fbd97
Lucas Lima <lucasrlima@gmail.com>
2023-11-03 16:57:32 +0000

    password size input added

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ function App() {
     const [password, setPassword] = useState("Get a random Password")
     const [copyText, setCopyText] = useState("Copy")
     const [buttonClassName, setButtonClassName] = useState(styles.wrapper)
+    const [passwordSize, setPasswordSize] = useState(12)
 
     function generate() {
       const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
-      const length = 12
       let newPassword = ""
-      for (let i = 0; i < length; i++) {
+      for (let i = 0; i < passwordSize && i < 99; i++) {
         const position = Math.floor(Math.random() * characters.length)
         newPassword += characters[position]
     }
@@ -35,6 +35,18 @@ function App() {
     <>
       <div className='container'>
       <h1>Password Generator</h1>
+        <div>
+          <label htmlFor="passwordSize">Password Size:</label>
+          <input 
+            className={styles.wrapper}
+            type="number" 
+            id='passwordSize' 
+            min={5}
+            max={99}
+            value={passwordSize}
+            onChange={ (ev) => setPasswordSize(ev.target.value)  }
+             />
+        </div>
         <div className="button-container">
           <Button Text={copyText} onClick={copyToClipboard} className={buttonClassName}></Button>
           <Button Text="Generate" onClick={generate} ></Button>
